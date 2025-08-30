ecf6b2a374b16d3092c4bd8422ac4fcbf2223da0
Emanuel Posescu <kind3r@gmail.com>
2025-08-30 21:06:11 +0300

    Debug route handler 2.

--- app/api/auth/[...civicauth]/route.ts
+++ app/api/auth/[...civicauth]/route.ts
@@ -1,14 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 import { handler } from "@civic/auth-web3/nextjs";
 
-const civic = handler();
+// const civic = handler();
 
 // export const GET = civic;
 
 export async function GET(req: NextRequest) {
   try {
     console.log("Civic GET request received");
-    return await civic(req);
+    return await handler()(req);
   } catch (e) {
     console.error("Civic GET error:", e);
     return NextResponse.json({ error: "Civic GET failed" }, { status: 500 });
